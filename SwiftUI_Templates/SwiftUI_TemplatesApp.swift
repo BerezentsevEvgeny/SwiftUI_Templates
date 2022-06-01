d9dfdb875f695d57eda2d8816835863ae4d2eb6e
Evgeniy Berezencev <evgeniy@abdigital.uz>
2022-06-01 22:45:23 +0300

    Initial Commit

---
+++ SwiftUI_Templates/SwiftUI_TemplatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TemplatesApp.swift
+//  SwiftUI_Templates
+//
+//  Created by Евгений Березенцев on 01.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TemplatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
